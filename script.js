5f8253bb41f516deaef34e1a9a43b27821788cfd
ZachLTech <zlopezzl2021@gmail.com>
2023-07-27 03:50:39 -0400

    new file:   assets/filter-black.svg 	new file:   assets/filter-white.svg 	deleted:    assets/filter.svg 	modified:   index.html 	modified:   script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-let lightdark = document.querySelector("#LightDark");
 let htmltag = document.querySelector("#htmltag");
+let filterSVG = document.querySelector("#filter-SVG");
 
 function check(){
     htmltag.classList.toggle("light");
+    filterSVG.classList.toggle("light");
     htmltag.setAttribute("data-theme", htmltag.classList.contains("light") ? "emerald" : "business");
+    filterSVG.setAttribute("src", htmltag.classList.contains("light") ? "assets/filter-black.svg" : "assets/filter-white.svg");
 }
-
